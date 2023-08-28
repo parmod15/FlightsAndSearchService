d1c4878db7f8e0a91a7019a55e53eb305f3aa4a2
parmod <parmodmagotra6391@gmail.com>
2023-08-28 10:27:43 +0530

    completed city-repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,9 +3,13 @@ const { City } = require("../models/index");
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = await City.create({
+        name,
+        // or name:name,
+      });
       return city;
     } catch (error) {
+      console.log("Something went wrong in repository layer");
       throw { error };
     }
   }
@@ -17,7 +21,32 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log("Something went wrong in repository layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong in repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in repository layer");
       throw { error };
     }
   }
