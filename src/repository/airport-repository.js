e6de5985df874f90a4015d025dcaae7472cb01c3
parmod <parmodmagotra6391@gmail.com>
2023-08-30 17:47:46 +0530

    completed airport repository

--- src/repository/airport-repository.js
+++ src/repository/airport-repository.js
@@ -26,6 +26,31 @@ class AirportRepository {
       throw { error };
     }
   }
+
+  async updateAirport(airportId, data) {
+    try {
+      const airport = await Airport.update(data, {
+        where: {
+          id: airportId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong in repository later");
+      throw { error };
+    }
+  }
+
+  async getAirport(airportId) {
+    try {
+      const airport = await Airport.findByPk(airportId);
+      airport.name = data.name;
+      await airport.save();
+      return airport;
+    } catch (error) {
+      console.log("Something went wrong in repository layer");
+      throw { error };
+    }
+  }
 }
 
 module.exports = AirportRepository;
